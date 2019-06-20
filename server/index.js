30de1a57e7ef5b58840a0a607e4d47e828f2fcdc
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-20 16:33:17 -0600

    db and server running

--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
 require('dotenv').config()
 const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
 const app = express()
-
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
 app.use(express.json())
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}))
+
+
+
+massive(CONNECTION_STRING).then((database) => {
+    app.set('db', database)
+    console.log('Database is running 👌', database.listTables())
+    app.listen(SERVER_PORT, () => console.log(`Speeding through port ${SERVER_PORT}`))
+})

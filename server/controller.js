c1c61a7b74253058edb1a4302d2232553d0b126e
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-20 15:46:32 -0600

    update register

---
+++ server/controller.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  register: async (req, res) => {
+    const { username, password, profile } = req.body;
+    const db = req.app.get("db");
+    const { session } = req;
+    const userFound = await db.check_username({ username });
+    if (userFound[0]) return res.status(409).send('Username already exists')
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const createUser = await db.register_user({ username, password: hash, profile})
+    session.user = { id: createUser[0].user_id, username: createUser[0].username, profile: createUser[0].profile }
+    res.status(200).send(session.user)
+  },
+  
+};

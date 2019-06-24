3e4a0b5e2a973fb8e485902ec8252896ed9b4a21
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-24 15:11:13 -0600

    update dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import * as Icon from "react-feather";
+
+import "./Dashboard.css";
 
 export default class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                Dashboard
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="dashboard-console">
+        <div className="avatar">
+          <div className="user-display">USER</div>
+        </div>
+        <div className="home-icon">
+          <Icon.Home size={40} />
+        </div>
+        <div className="add-icon">
+          <Icon.FilePlus size={40} />
+        </div>
+        <div className="power-icon">
+          <Icon.Power size={40} />
+        </div>
+      </div>
+    );
+  }
 }

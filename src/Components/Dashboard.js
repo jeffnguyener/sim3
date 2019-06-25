9cf11e22832adbe125ea8bc78e7f630356667aa8
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-25 14:54:09 -0600

    Moved everything to nav

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,32 +1,12 @@
 import React, { Component } from "react";
-import * as Icon from "react-feather";
-import { Link } from 'react-router-dom'
 
 import "./Dashboard.css";
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div className="dashboard-console">
-        <div className="avatar">
-          <div className="user-display">USER</div>
+        <div className="dashboard-console">
         </div>
-        <div className="home-icon">
-          <Link to="/dashboard">
-          <Icon.Home size={40} className="icons" />
-          </Link>
-        </div>
-        <div className="add-icon">
-          <Link to="/post">
-          <Icon.FilePlus size={40} className="icons" />
-          </Link>
-        </div>
-        <div className="power-icon">
-          <Link to="/auth">
-          <Icon.Power size={40} className="icons" />
-          </Link>
-        </div>
-      </div>
     );
   }
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import * as Icon from "react-feather";
 
 import "./Nav.css";
 
 export default class Nav extends Component {
   render() {
     return (
-      <div className="navbar">
-        {/* <div className="nav-links">
-          <Link to="/dashboard">Home</Link>
-          <Link to="/post">New Post</Link>
-          <Link to="/">Logout</Link>
-        </div> */}
+      <div className="nav-body">
+        <div className="navbar">
+          <div className="avatar">
+            <div className="user-display">USER</div>
+          </div>
+          <div className="home-icon">
+            <Link to="/dashboard">
+              <Icon.Home size={40} className="icons" />
+            </Link>
+          </div>
+          <div className="add-icon">
+            <Link to="/post">
+              <Icon.FilePlus size={40} className="icons" />
+            </Link>
+          </div>
+          <div className="power-icon">
+            <Link to="/auth">
+              <Icon.Power size={40} className="icons" />
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }

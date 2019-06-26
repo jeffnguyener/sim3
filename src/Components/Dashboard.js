cdc867d98ad3f2af54871f02144f122714e3d76a
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-26 09:57:42 -0600

    Adjusted search bar and post

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -5,13 +5,15 @@ import "./Dashboard.css";
 export default class Dashboard extends Component {
   render() {
     return (
-        <div className="dashContainer">
-          <div className="dash-console">
+      <div className="dashContainer">
+        <div className="dash-console">
           <div className="dash-search" />
-            <div className="dash-profile" />
-          </div>
-          <div className="post-container" />
         </div>
+        <div className="post-container">
+          {}
+          <div className="dash-profile" />
+        </div>
+      </div>
     );
   }
 }

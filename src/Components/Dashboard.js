654e46dcfc4839af554b79515c3b1c2c5fa20e86
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-06-25 21:16:00 -0600

    Adjusted css

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,7 +6,10 @@ export default class Dashboard extends Component {
   render() {
     return (
         <div className="dashContainer">
-          <div className="dash-console" />
+          <div className="dash-console">
+          <div className="dash-search" />
+            <div className="dash-profile" />
+          </div>
           <div className="post-container" />
         </div>
     );
